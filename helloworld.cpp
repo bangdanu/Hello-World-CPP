28a528e80a78291429511590a05f61b0a282b889
bangdanu <38716186+bangdanu@users.noreply.github.com>
2023-03-14 21:00:44 +0700

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  cout<<"Hello Dunia!!!"<< endl;
+  cout<<"Nama Saya Tantri Hidayati"<< endl;
+  cout<<"Usia saya 38 tahun";
+  return 0;
+
+}

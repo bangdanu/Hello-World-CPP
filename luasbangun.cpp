937766ccfa355169b77eb3689c21f0efe75ea9db
bangdanu <38716186+bangdanu@users.noreply.github.com>
2023-03-14 21:15:10 +0700

    Create luasbangun.cpp

---
+++ luasbangun.cpp
@@ -0,0 +1,9 @@
+#include <iostream.h> 
+main(){ 
+float panjang, lebar, luas; 
+cout << ”Panjang : ” ; cin >> panjang; 
+cout << ”Lebar : ” ; cin >> lebar; 
+
+luas = panjang * lebar; 
+cout << ”Luas : ” << luas << endl; 
+}
